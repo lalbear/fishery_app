6b4361c9c9c21028d9476bb186ba30684be4294e
Pranjal Upadhyay <pranjalup25@gmail.com>
2026-03-05 23:45:07 +0530

    fix(mobile): resolve i18n language detector type error and apply persistent language

--- mobile/src/i18n/index.ts
+++ mobile/src/i18n/index.ts
@@ -3,10 +3,10 @@
  * Multi-language support for rural Indian farmers
  */
 
-
-import i18n from 'i18next';
+import i18n, { LanguageDetectorAsyncModule } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import * as Localization from 'expo-localization';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Import translations
 import en from './locales/en.json';
@@ -17,12 +17,35 @@ const resources = {
   hi: { translation: hi },
 };
 
+const languageDetector: LanguageDetectorAsyncModule = {
+  type: 'languageDetector',
+  async: true,
+  detect: async (callback: (lng: string | readonly string[] | undefined) => void | undefined) => {
+    try {
+      const savedLanguage = await AsyncStorage.getItem('@fishing_god_language');
+      if (savedLanguage) {
+        callback(savedLanguage);
+        return savedLanguage;
+      }
+    } catch (e) { }
+    const defaultLng = Localization.getLocales()[0]?.languageCode || 'en';
+    callback(defaultLng);
+    return defaultLng;
+  },
+  init: () => { },
+  cacheUserLanguage: async (lng: string) => {
+    try {
+      await AsyncStorage.setItem('@fishing_god_language', lng);
+    } catch (e) { }
+  },
+};
+
 i18n
+  .use(languageDetector)
   .use(initReactI18next)
   .init({
     resources,
     compatibilityJSON: 'v3',
-    lng: Localization.getLocales()[0]?.languageCode || 'en',
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
